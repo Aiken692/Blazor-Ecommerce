65fa71b11615d1b32ffcf2e7736d2680334b1fbc
Rolland\EmataTemp <walter@emata.io>
2023-09-03 00:41:56 +0300

    - Using ProductService in the ProductList Component

---
+++ BlazorEcommerce/Client/Services/ProductService/ProductServoce.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace BlazorEcommerce.Client.Services.ProductService
+{
+    public class ProductServoce : IProductService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ProductServoce(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public async Task GetProducts()
+        {
+            var result = await _httpClient.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/products");
+
+            if (result != null && result.Data != null)
+            {
+                Products = result.Data;
+            }
+        }
+
+    }
+}
