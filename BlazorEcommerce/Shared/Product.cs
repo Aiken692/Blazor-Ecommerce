c8273155f8c9a2ab7f81e18681ea760e0991ea05
LAPTOP-2SN37O2L\rwalt <walter@emata.io>
2023-10-11 12:31:09 +0300

    ChangedGuidstoInt_In_Products_And_Category

--- BlazorEcommerce/Shared/Product.cs
+++ BlazorEcommerce/Shared/Product.cs
@@ -2,13 +2,13 @@
 {
     public class Product
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string? Title { get; set; }
 
         public string? Description { get; set; }
         public double Price { get; set;}
         public string? ImageUrl { get; set; }
-        public Guid CategoryId { get; set; }
+        public int CategoryId { get; set; }
         public Category? Category { get; set; }
     }
 }
