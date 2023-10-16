1e9753eef429b7934d824758e4d5c00ca1a1c8e2
LAPTOP-2SN37O2L\rwalt <walter@emata.io>
2023-10-16 11:01:52 +0300

    Added logic to Change Password both back end and fromt end

--- BlazorEcommerce/Client/Services/AuthService/AuthService.cs
+++ BlazorEcommerce/Client/Services/AuthService/AuthService.cs
@@ -9,6 +9,13 @@
             _httpClient = httpClient;
         }
 
+        public async Task<ServiceResponse<bool>> ChangePaaword(UserChangePassword request)
+        {
+            var result = await _httpClient.PostAsJsonAsync("api/auth/change-password", request.Password);
+
+            return await result.Content.ReadFromJsonAsync<ServiceResponse<bool>>();
+        }
+
         public async Task<ServiceResponse<string>> Login(UserLogin request)
         {
             var result = await _httpClient.PostAsJsonAsync("api/auth/login", request);
